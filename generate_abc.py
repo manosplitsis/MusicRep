720f8643cc408be649e92f5fd6b729b1f05e8b0c
manosplitsis <38662795+manosplitsis@users.noreply.github.com>
2020-08-27 16:28:45 +0300

    update

--- generate_abc.py
+++ generate_abc.py
@@ -91,7 +91,7 @@ def generate_text(model_path,text_path,seed_ind,seq_length,gen_length):
     print(output)
     return(output_ind,output)
 
-def generate_abc_naive(model_path,text_path,seq_length,temp=1.,no_exports=1):
+def generate_abc_naive(model_path,text_path,seed_ind,seq_length,generate_pieces=True,temp=1.,no_exports=1):
     
     
     model=load_model(model_path)
@@ -100,8 +100,8 @@ def generate_abc_naive(model_path,text_path,seq_length,temp=1.,no_exports=1):
     #start_index = random.randint(0, len(text) - seq_length - 1)
     start_index=0
     pieces=text.split('\n\n')
-    pieces_c=pieces[:22925]
-    pieces_csharp=pieces[22925:]
+    pieces_c=pieces[:12117]
+    pieces_csharp=pieces[12117:]
     
     del text
     val_split=0.1
@@ -114,7 +114,7 @@ def generate_abc_naive(model_path,text_path,seq_length,temp=1.,no_exports=1):
     pieces_train=pieces_train_c+pieces_train_csharp
     pieces_validate=pieces_validate_c+pieces_validate_csharp
     del pieces
-    sentence = pieces_validate_c[0].split()[start_index: start_index + seq_length]
+    sentence = pieces_validate_c[seed_ind].split()[start_index: start_index + seq_length]
     generated=[]
     
     #generated=np.array(generated)
@@ -131,11 +131,11 @@ def generate_abc_naive(model_path,text_path,seq_length,temp=1.,no_exports=1):
     print('----- Generating with seed: "' + ''.join(sentence)+ '"')
     #sys.stdout.write(generated)
     if generate_pieces:
-        for i in no_exports:
+        for i in range(no_exports):
             abc=''
             stop=False
             count=0
-            count_abc
+            count_abc=0
             while stop==False and count<output_length:
                 x_pred=np.array(generated)
         
@@ -180,26 +180,73 @@ def make_abc_seeds(text_path,no_seeds,seq_length):
     #start_index = random.randint(0, len(text) - seq_length - 1)
     start_index=0
     pieces=text.split('\n\n')
-    pieces_c=pieces[:22925]
-    pieces_csharp=pieces[22925:]
+    #pieces_c=pieces[:12117]
+    #pieces_csharp=pieces[12117:]
     
     del text
     val_split=0.1
     
     
-    pieces_train_c=pieces_c[0:len(pieces_c)-int(val_split*len(pieces_c))]
+    #pieces_train_c=pieces_c[0:len(pieces_c)-int(val_split*len(pieces_c))]
     pieces_validate_c=pieces_c[len(pieces_c)-int(val_split*len(pieces_c)):len(pieces_c)]
-    pieces_train_csharp=pieces_csharp[0:len(pieces_csharp)-int(val_split*len(pieces_csharp))]
-    pieces_validate_csharp=pieces_csharp[len(pieces_csharp)-int(val_split*len(pieces_csharp)):len(pieces_csharp)]
-    pieces_train=pieces_train_c+pieces_train_csharp
-    pieces_validate=pieces_validate_c+pieces_validate_csharp
+    #pieces_train_csharp=pieces_csharp[0:len(pieces_csharp)-int(val_split*len(pieces_csharp))]
+    #pieces_validate_csharp=pieces_csharp[len(pieces_csharp)-int(val_split*len(pieces_csharp)):len(pieces_csharp)]
+    #pieces_train=pieces_train_c+pieces_train_csharp
+    #pieces_validate=pieces_validate_c+pieces_validate_csharp
     del pieces
-    inds=np.random.randint(0,len(pieces_validate_c),size=no_seeds)
+    #inds=np.random.randint(0,len(pieces_validate),size=no_seeds)
+    inds=pieces_validate[:no_seeds]
     seeds=[]
     for i in inds:
         seeds.append(pieces_validate_c[i].split()[start_index: start_index + seq_length])
     return seeds
 
+def make_abc_seeds_bars(text_path,no_seeds,bar_length):
+    print('making seeds of bar length: ',bar_length)
+    text=load_doc(text_path)
+    chars = sorted(list(set(text)))
+    #start_index = random.randint(0, len(text) - seq_length - 1)
+    start_index=0
+    pieces=text.split('\n\n')
+    #pieces_c=pieces[:12117]
+    #pieces_csharp=pieces[12117:]
+    
+    del text
+    val_split=0.1
+    
+    
+    #pieces_train_c=pieces_c[0:len(pieces_c)-int(val_split*len(pieces_c))]
+    pieces_validate=pieces[len(pieces)-int(val_split*len(pieces)):len(pieces)]
+    print('choosing from ',len(pieces_validate),'pieces')
+    #pieces_train_csharp=pieces_csharp[0:len(pieces_csharp)-int(val_split*len(pieces_csharp))]
+    #pieces_validate_csharp=pieces_csharp[len(pieces_csharp)-int(val_split*len(pieces_csharp)):len(pieces_csharp)]
+    #pieces_train=pieces_train_c+pieces_train_csharp
+    #pieces_validate=pieces_validate_c+pieces_validate_csharp
+    del pieces
+    #inds=np.random.randint(0,len(pieces_validate),size=no_seeds)
+    inds=np.arange(no_seeds)
+    seeds=[]
+    for i in inds:
+        seed_piece=pieces_validate[i]
+        seed=cut_after_bars(seed_piece, bar_length)
+        seeds.append(seed)
+    return seeds
+#%%
+def cut_after_bars(abc,bars):
+    abc=abc.split()
+    count_bars=0
+    if not abc[0]=='<s>':
+        print('no start symbol')
+    if '|' in abc[1]:
+        bars+=1
+    for i in range(len(abc)):
+        if '|' in abc[i]:
+            count_bars+=1
+        if count_bars>=bars:
+            break
+        
+    return abc[:i+1]
+#%%
 def generate_many_abc(model_path,seeds,seq_length,temp=1.,no_exports=1,generate_pieces=True,output_length=500):
     
     
@@ -211,6 +258,7 @@ def generate_many_abc(model_path,seeds,seq_length,temp=1.,no_exports=1,generate_
     char_indices=dictionary
     indices_char= {value:key for (key,value) in dictionary.items()}
     sentences=[]
+    seed_length=len(seeds[0])
     for seed in seeds:
         sentence=[]
         for i in seed:
@@ -232,7 +280,7 @@ def generate_many_abc(model_path,seeds,seq_length,temp=1.,no_exports=1,generate_
         while len(sentences)>0 and count<output_length:
             
             if delete_sentence>=0:
-                print('deleting sentence ',delete_sentence, 'with length ',len(sentences[delete_sentence]))
+                #print('deleting sentence ',delete_sentence, 'with length ',len(sentences[delete_sentence]))
                 sentences.pop(delete_sentence)
                 delete_sentence=-1
                 if len(sentences)==0:
@@ -245,22 +293,23 @@ def generate_many_abc(model_path,seeds,seq_length,temp=1.,no_exports=1,generate_
                 next_char = indices_char[next_index]
                 
                 if next_char=='</s>':
-                    print('to be deleted:',i)
+                    #print('to be deleted:',i)
                     stop=True
                     
                     piece=[indices_char[ind] for ind in sentences[i] ]
                     if len(abc)==0:
                         abc=process_abc(piece,count_abc)
-                        print(abc)
+                        
                     else:
                         abc+='\n\n'+process_abc(piece,count_abc)
-                        print(abc)
+                        
                     count_abc+=1
                     delete_sentence=i
                     print('tunes made:',count_abc)
                     #sentences=np.delete(sentences,i,axis=0) #remove from seeds
                     #sentences.pop(i)
                     continue
+                
                 if len(sentences)>0:
                     sentences[i]=np.append(sentences[i],next_index)
         if count<output_length:
@@ -269,6 +318,10 @@ def generate_many_abc(model_path,seeds,seq_length,temp=1.,no_exports=1,generate_
                 abc+='\n\n'+process_abc(piece,count_abc)
                 count_abc+=1
                 sentences=np.delete(sentences,i,axis=0)
+        print(abc)        
+        with open(experiment_path+'/output_seedlen_'+str(seed_length)+'.abc','w') as f:
+            f.write(abc)
+        print('saved at: '+experiment_path)
     else:
         for i in range(output_length):
             x_pred=np.array(generated)
@@ -287,6 +340,52 @@ def generate_many_abc(model_path,seeds,seq_length,temp=1.,no_exports=1,generate_
     
     return(abc)
 
+def generate_bars(model_path,seeds,temp=1.,generate_pieces=True,output_bars=8):
+    
+    
+    model=load_model(model_path)
+    experiment_path=os.path.dirname(os.path.dirname(model_path))
+    
+    dictionary=np.load(experiment_path+'/dictionary',allow_pickle=True)
+    n_vocab=len(dictionary)
+    char_indices=dictionary
+    indices_char= {value:key for (key,value) in dictionary.items()}
+    sentences=[]
+    seed_length=1 #1 bar seeds
+    count_abc=0
+    for seed in seeds:
+        sentence=[]
+        for i in seed:
+            sentence.append(dictionary[i])
+        sentences.append(np.array(sentence))
+    #sentences=np.array(sentences,dtype='obj')
+    #generated=[]
+    #generated = sentence
+    #print('----- Generating with seed: "' + ''.join(sentence)+ '"')
+    #sys.stdout.write(generated)
+    abc=''
+    for i in range(len(sentences)):
+        count_bars=seed_length
+        while count_bars<=output_bars:
+            sentence=sentences[i]
+            x_pred=np.reshape(sentences[i],(1,len(sentences[i])))
+            preds=model.predict(x_pred)
+            next_index=sample(preds[0][-1],temp)
+            next_char=indices_char[next_index]
+            if next_char=='</s>' or next_char=='<s>':
+                continue
+            if '|' in next_char:
+                count_bars+=1
+            sentences[i]=np.append(sentences[i],next_index)
+            seeds[i].append(next_char)
+        abc+='\n\n'+process_abc(seeds[i],count_abc)
+        count_abc+=1
+    print(abc)
+    with open(experiment_path+'/output_seedlenbars_'+str(seed_length)+'.abc','w') as f:
+            f.write(abc)
+    print('saved at: '+experiment_path)
+
+
 def process_abc(sentence,count_abc):
     abc='X:'+str(count_abc)+'\nM:4/4\nK:Cmaj\n'
     for i in sentence[1:]:
@@ -303,9 +402,12 @@ def process_abc2(sentence,count_abc):
 
   
 if __name__=='__main__':
-    model_path='experiments/seq_song/ABC/data_V3_nohead_model_n1_s32_d0.2_sl100_bs256_C_run_1111115/models/model-022-1.7627-1.6856'
-    text_path='data/data_V3_nohead'
-    no_seeds=5
-    seq_length=10
-    seeds=make_abc_seeds(text_path, no_seeds, seq_length)
-    tt=generate_many_abc(model_path, seeds, seq_length)
+    model_path='models/data_V3_nohead_model_n1_s32_d0.2_sl100_bs256_C_run_0/models/model-164-1.7136-1.6604'
+    text_path='data/text_44_nohead'
+    no_seeds=200
+    seq_length=1
+    bar_length=1
+    #seeds=make_abc_seeds(text_path, no_seeds, seq_length)
+    #tt=generate_many_abc(model_path, seeds, seq_length)
+    seeds=make_abc_seeds_bars(text_path, no_seeds, bar_length)
+    generate_bars(model_path,seeds,output_bars=8)
